e0647125b188a470536eabcd164aea0a1194a720
Ahmet Utlu <ahmet.utlu@basefy.com>
2024-02-12 14:33:11 +0100

    Initial Commit

---
+++ Sandstorm/SandstormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandstormApp.swift
+//  Sandstorm
+//
+//  Created by Ahmet Utlu on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SandstormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
